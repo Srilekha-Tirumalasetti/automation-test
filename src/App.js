49c39ffdc4b236ad964a6176a602e858d267ce35
Srilekha-Tirumalasetti <srilekha@auroraelabs.com>
2024-05-06 14:42:36 +0530

    random-change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learnnnnn
+          Learnnnnn react
         </a>
       </header>
     </div>
